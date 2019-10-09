b848f9a42fc99cb6a14b8d8c2b039ce1fc8aabdf
Ronnie Payne <rpayne@sitetracker.com>
2019-10-09 10:56:30 -0400

    initial Commit

---
+++ boilerplate/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const docRoot = document.getElementById( "app" );
+ReactDOM.render( <div>boilerplate</div>, docRoot );
